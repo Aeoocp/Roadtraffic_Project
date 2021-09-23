d3be86d260e97da523d1aa6145c7ee3399bf2dd5
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-09-23 09:10:51 +0700

    Create import_txt.py

---
+++ tools/import_txt.py
@@ -0,0 +1,54 @@
+def split_txt(file)
+  file1 = open(file, "r")
+  FileContent = file1.read()
+  #print(FileContent)
+  AS = FileContent.split()
+  inform = []
+  car_num = []
+
+  c = -1
+  y = 0
+  for x in range(len(AS)-1):
+    xx = AS[x].split(',')
+    if y == 0:
+      if AS[x] == "#":
+        c = c+1
+        car_num.append(AS[x+3])
+        skip = x+3
+        inform.append([])
+      elif len(xx) == 2:
+        if xx[1] != '':
+          y = 1
+          inform[c].append(AS[x]+AS[x+1])
+      elif  x > skip:
+        inform[c].append(AS[x])
+    else:
+        y = 0
+
+  boxes_s = []
+  confidence_s = []
+  classes_s = []
+
+  for a in range(len(inform)):
+    subclass = [] 
+    subconfidence = []
+    subbox = []
+    for b in range(len(inform[a])):
+      eachBox = inform[a][b]
+      SEachBox = eachBox.split(',')
+      Frame = SEachBox[0]
+      subclass.append(SEachBox[1])
+      subconfidence.append(float(SEachBox[6]))
+      ssubbox = []
+      ssubbox.append(float(SEachBox[2]))
+      ssubbox.append(float(SEachBox[3]))
+      w = float(SEachBox[4]) - float(SEachBox[2])
+      h = float(SEachBox[5]) - float(SEachBox[3])
+      ssubbox.append(w)
+      ssubbox.append(h)
+      subbox.append(ssubbox)
+      classes_s.append(subclass)
+      confidence_s.append(subconfidence)
+      boxes_s.append(subbox)
+      
+  return classes_s,confidence_s,boxes_s

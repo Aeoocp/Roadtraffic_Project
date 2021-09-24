b2e54759e3d9db56f8387ae4a51cca5109c5a4d7
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-09-24 10:07:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -164,4 +164,179 @@ def main(_argv):
       for track in tracker.tracks:
           if not track.is_confirmed() or track.time_since_update > 1:
             continue
+          bbox = track.to_tlbr()
+          # most common detection class for track
+          track_cls = track.cls  
+          
+          midpoint = track.tlbr_midpoint(bbox)
+          # get midpoint respective to botton-left
+          origin_midpoint = (midpoint[0], frame.shape[0] - midpoint[1])
+
+          if track.track_id not in memory:
+              memory[track.track_id] = deque(maxlen=2)
+              
+          memory[track.track_id].append(midpoint)
+          previous_midpoint = memory[track.track_id][0]
+          
+          origin_previous_midpoint = (previous_midpoint[0], frame.shape[0] - previous_midpoint[1])
+
+          cv2.line(frame, midpoint, previous_midpoint, (0, 255, 0), 2)
+          
+          # Add to counter and get intersection details
+          A = midpoint
+          B = previous_midpoint
+          C = line[0]
+          D = line[1]
+          ccw1 = (D[1] - A[1]) * (C[0] - A[0]) > (C[1] - A[1]) * (D[0] - A[0])
+          ccw2 = (D[1] - B[1]) * (C[0] - B[0]) > (C[1] - B[1]) * (D[0] - B[0])
+          ccw3 = (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
+          ccw4 = (D[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (D[0] - A[0])
+          if (ccw1 != ccw2 and ccw3 != ccw4) and track.track_id not in already_counted:
+            class_counter[track_cls] += 1
+            total_counter += 1
+            # draw red line
+            cv2.line(frame, line[0], line[1], (0, 0, 255), 2)
+
+            # Set already counted for ID to true.
+            already_counted.append(track.track_id) 
+
+            intersection_time = datetime.datetime.now() - datetime.timedelta(microseconds=datetime.datetime.now().microsecond)
+            mx = origin_midpoint[0] - origin_previous_midpoint[0]
+            my = origin_midpoint[1] - origin_previous_midpoint[1] 
+            angle = Camera.vector_angle(origin_midpoint, origin_previous_midpoint)
+            intersect_info.append([track_cls, origin_midpoint, angle, intersection_time])
+
+            if angle > 0:
+              up_count += 1
+            if angle < 0:
+              down_count += 1
+            
+          # WHITE BOX
+          cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)  
+          cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 1.5e-3 * frame.shape[0], (0, 255, 0), 2)
+
+          if not show_detections:
+            # Average detection confidence
+            adc = "%.2f" % (track.adc * 100) + "%" 
+            cv2.putText(frame, str(track_cls), (int(bbox[0]), int(bbox[3])), 0, 1e-3 * frame.shape[0], (0, 255, 0), 2)
+            cv2.putText(frame, 'ADC: ' + adc, (int(bbox[0]), int(bbox[3] + 2e-2 * frame.shape[1])), 0, 1e-3 * frame.shape[0], (0, 255, 0), 2)
+          
+          
+        # Delete memory of old tracks.
+        # This needs to be larger than the number of tracked objects in the frame.
+        if len(memory) > 50:
+            del memory[list(memory)[0]]
+            
+        # Draw total count.
+        cv2.putText(frame, "Total: {} ({} up, {} down)".format(str(total_counter), str(up_count), str(down_count)), (int(0.05 * frame.shape[1]), int(0.1 * frame.shape[0])), 0, 1.5e-3 * frame.shape[0], (0, 255, 255), 2)
+          
+        if show_detections:
+          for det in detections:
+            bbox = det.to_tlbr()
+            score = "%.2f" % (det.confidence * 100) + "%"
+            # BLUE BOX
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 2)  
+            if len(classes) > 0:
+              det_cls = det.cls
+              if det_cls == "car":
+                cv2.putText(frame, str(det_cls) + " " + score, (int(bbox[0]), int(bbox[3])), 0, 1.5e-3 * frame.shape[0], (0, 255, 0), 2)
+              else:
+                cv2.putText(frame, str(det_cls) + " " + score, (int(bbox[0]), int(bbox[3])), 0, 1.5e-3 * frame.shape[0], (0, 0, 255), 2)
+                
+        # display counts for each class as they appear
+        y = 0.2 * frame.shape[0]
+        for cls in class_counter:
+          class_count = class_counter[cls]
+          cv2.putText(frame, str(cls) + " " + str(class_count), (int(0.05 * frame.shape[1]), int(y)), 0, 1.5e-3 * frame.shape[0], (0, 255, 255), 2)
+          y += 0.05 * frame.shape[0]
+          
+        # calculate current minute
+        now = datetime.datetime.now()
+        rounded_now = now - datetime.timedelta(microseconds=now.microsecond)  # round to nearest second
+        current_minute = now.time().minute
+        
+        if current_minute == 0 and len(count_dict) > 1:
+          count_dict = {}  # reset counts every hour
+        else:
+          # write counts to file for every set interval of the hour
+          write_interval = 5
+          if current_minute % write_interval == 0:  # write to file once only every write_interval minutes
+            if current_minute not in count_dict:
+              count_dict[current_minute] = True
+              total_filename = 'Total counts for {}, {}.txt'.format(current_date, cam_id)
+              counts_folder = './counts/'
+              if not os.access(counts_folder + str(current_date) + '/total', os.W_OK):
+                os.makedirs(counts_folder + str(current_date) + '/total')
+              total_count_file = open(counts_folder + str(current_date) + '/total/' + total_filename, 'a')
+              print('{} writing...'.format(rounded_now))
+              print('Writing current total count ({}) and directional counts to file.'.format(total_counter))
+              total_count_file.write('{}, {}, {}, {}, {}\n'.format(str(rounded_now), device, str(total_counter), up_count, down_count))
+              total_count_file.close()
+              
+              # if class exists in class counter, create file and write counts
+              if not os.access(counts_folder + str(current_date) + '/classes', os.W_OK):
+                os.makedirs(counts_folder + str(current_date) + '/classes')
+              for cls in class_counter:
+                class_count = class_counter[cls]
+                print('Writing current {} count ({}) to file.'.format(cls, class_count))
+                class_filename = 'Class counts for {}, {}.txt'.format(current_date, cam_id)
+                class_count_file = open(counts_folder + str(current_date) + '/classes/' + class_filename, 'a')
+                class_count_file.write("{}, {}, {}\n".format(rounded_now, device, str(class_count)))
+                class_count_file.close()
+                
+              # write intersection details
+              if not os.access(counts_folder + str(current_date) + '/intersections', os.W_OK):
+                os.makedirs(counts_folder + str(current_date) + '/intersections')
+              print('Writing intersection details for {}'.format(cam_id))
+              intersection_filename = 'Intersection details for {}, {}.txt'.format(current_date, cam_id)
+              intersection_file = open(counts_folder + str(current_date) + '/intersections/' + intersection_filename, 'a')
+              for i in intersect_info:
+                cls = i[0]
+                
+                midpoint = i[1]
+                x = midpoint[0]
+                y = midpoint[1]
+                
+                angle = i[2]
+                
+                intersect_time = i[3]
+                
+                intersection_file.write("{}, {}, {}, {}, {}, {}\n".format(str(intersect_time), device, cls, x, y, str(angle)))
+
+              intersection_file.close()
+              intersect_info = []  # reset list after writing
+
+      if writeVideo_flag:
+        # save a frame
+        out.write(frame)
+        frame_index = frame_index + 1
+        
+      fps_imutils.update()
+      
+      if not asyncVideo_flag:
+        fps = (fps + (1. / (time.time() - t1))) / 2
+        #print("FPS = %f" % (fps))
+        
+      # Press Q to stop!
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+    fps_imutils.stop()
+    print('imutils FPS: {}'.format(fps_imutils.fps()))
+      
+    if asyncVideo_flag:
+      video_capture.stop()
+    else:
+      video_capture.release()  
+    
+    if writeVideo_flag:
+      out.release()
+        
+    cv2.destroyAllWindows()
+    
+if __name__ == '__main__':
+  try:
+    app.run(main)
+  except SystemExit:
+    pass
           

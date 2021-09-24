975599e7d95920c0b8935ff6bbd807b04d39bc10
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-09-24 10:22:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ from tools import import_txt
 import imutils.video
 from videocaptureasync import VideoCaptureAsync
 
+from collections import Counter
+from collections import deque 
+import datetime
+import math
+
 warnings.filterwarnings('ignore')
 
 def main(_argv):

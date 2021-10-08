ca997c44919da8c071b35b357fdecb848a3aca1c
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-10-08 15:37:55 +0700

    Update newwork.py

--- newwork.py
+++ newwork.py
@@ -67,7 +67,7 @@ def main(_argv):
       w = int(video_capture.get(3))
       h = int(video_capture.get(4))
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi', fourcc, 30, (w, h))
+    out = cv2.VideoWriter('output_2line.avi', fourcc, 30, (w, h))
     frame_index = -1
 
   fps = 0.0
@@ -122,9 +122,9 @@ def main(_argv):
     frameY = frame.shape[0] #360
     frameX = frame.shape[1] #640
     line = [(int(0.3 * frameX), int(0.8 * frameY)), (int(0.55 * frameX), int(0.85 * frameY))]
-    cv2.line(frame, line[0], line[1], (0, 255, 255), 2)   #(image, start_point, end_point, color, thickness)
+    cv2.line(frame, line[0], line[1], (255, 255, 255), 2)   #(image, start_point, end_point, color, thickness)
     line2 = [(int(0.05 * frameX), int(0.6 * frameY)), (int(0.2 * frameX), int(0.65 * frameY))]
-    cv2.line(frame, line2[0], line2[1], (255, 0, 0), 2)   #(image, start_point, end_point, color, thickness)
+    cv2.line(frame, line2[0], line2[1], (255, 255, 255), 2)   #(image, start_point, end_point, color, thickness)
 
     for track in tracker.tracks:
       if not track.is_confirmed() or track.time_since_update > 1:
@@ -132,11 +132,8 @@ def main(_argv):
       bbox = track.to_tlbr()
       track_cls = track.cls
       
-      midpoint = track.tlbr_midpoint(bbox)
-      origin_midpoint = (midpoint[0], frame.shape[0] - midpoint[1])
-      
       if track_cls == "car":
-        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
+        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0)), 2)
         cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 1.5e-3 * frame.shape[0], (0, 255, 0), 1)
         cv2.putText(frame, str(track_cls), (int(bbox[0]), int(bbox[3])), 0, 1e-3 * frame.shape[0], (0, 255, 0), 1)
       elif track_cls == "truck":
@@ -147,6 +144,10 @@ def main(_argv):
         cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 2)
         cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 1.0e-3 * frame.shape[0], (255, 0, 0), 1)
         cv2.putText(frame, str(track_cls), (int(bbox[0]), int(bbox[3])), 0, 1e-3 * frame.shape[0], (255, 0, 0), 1)
+      
+      midpoint = track.tlbr_midpoint(bbox)
+      # get midpoint respective to botton-left
+      origin_midpoint = (midpoint[0], frame.shape[0] - midpoint[1])
 
       if track.track_id not in memory:
         memory[track.track_id] = deque(maxlen=2)  
@@ -154,18 +155,32 @@ def main(_argv):
       memory[track.track_id].append(midpoint)
       previous_midpoint = memory[track.track_id][0]
       origin_previous_midpoint = (previous_midpoint[0], frame.shape[0] - previous_midpoint[1])
-
-      cv2.line(frame, midpoint, previous_midpoint, (0, 255, 0), 2)
+      
+      if track_cls == "car":
+        cv2.line(frame, midpoint, previous_midpoint, (0, 255, 0), 2)
+      elif track_cls == "truck":
+        cv2.line(frame, midpoint, previous_midpoint, (0, 0, 255), 2)
+      else:
+        cv2.line(frame, midpoint, previous_midpoint, (255, 0, 0), 2)
       
       TC = CheckCrossLine.LineCrossing(midpoint, previous_midpoint, line[0] ,line[1])
+      TC2 = CheckCrossLine.LineCrossing(midpoint, previous_midpoint, line2[0] ,line2[1])
       if TC and (track.track_id not in already_counted):
         class_counter[track_cls] += 1
         total_counter += 1
-        # draw red line
-        cv2.line(frame, line[0], line[1], (0, 0, 255), 2)
+        # draw alert line
+        cv2.line(frame, line[0], line[1], (0, 255, 255), 2)
         already_counted.append(track.track_id)  # Set already counted for ID to true.
         intersection_time = datetime.datetime.now() - datetime.timedelta(microseconds=datetime.datetime.now().microsecond)
         intersect_info.append([track_cls, origin_midpoint, intersection_time])
+      elif TC2 and (track.track_id not in already_counted):
+        class_counter2[track_cls] += 1
+        total_counter2 += 1
+        # draw alert line
+        cv2.line(frame, line2[0], line2[1], (0, 255, 255), 2)
+        already_counted.append(track.track_id)  # Set already counted for ID to true.
+        intersection_time = datetime.datetime.now() - datetime.timedelta(microseconds=datetime.datetime.now().microsecond)
+        intersect_info2.append([track_cls, origin_midpoint, intersection_time])
         
     # Delete memory of old tracks.
     # This needs to be larger than the number of tracked objects in the frame.  
@@ -174,7 +189,9 @@ def main(_argv):
 
     # Draw total count.
     cv2.putText(frame, "Total: {}".format(str(total_counter)), (int(0.8 * frame.shape[1]), int(0.1 * frame.shape[0])), 0,
-                1.5e-3 * frame.shape[0], (0, 255, 255), 2)
+                1.5e-3 * frame.shape[0], (255, 255, 255), 2)
+    cv2.putText(frame, "Total2: {}".format(str(total_counter2)), (int(0.05 * frame.shape[1]), int(0.1 * frame.shape[0])), 0,
+                  1.5e-3 * frame.shape[0], (255, 255, 255), 2)
     
     # display counts for each class as they appear
     y = 0.2 * frame.shape[0]
@@ -184,6 +201,11 @@ def main(_argv):
       cv2.putText(frame, str(cls) + " " + str(class_count), (int(0.8 * frame.shape[1]), int(y)), 0,
                   1.5e-3 * frame.shape[0], (0, 255, 255), 2)
       y += 0.05 * frame.shape[0]
+    for cls in class_counter2:
+      class_count2 = class_counter2[cls]
+      cv2.putText(frame, str(cls) + " " + str(class_count2), (int(0.05 * frame.shape[1]), int(y2)), 0,
+                  1.5e-3 * frame.shape[0], (255, 0, 0), 2)
+      y2 += 0.05 * frame.shape[0]
         
     if writeVideo_flag:
         # save a frame

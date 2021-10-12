158dfb23a8eb01df982ec1072e447490b0b5d6e7
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-10-12 23:52:34 +0700

    Update Track2line_skip106.py

--- Track2line_skip106.py
+++ Track2line_skip106.py
@@ -118,6 +118,14 @@ def main(_argv):
       tracker.predict()
       tracker.update(detections)
 
+      #สร้างและวาดเส้นผ่าน
+      frameY = frame.shape[0] #360
+      frameX = frame.shape[1] #640
+      line = [(int(0.3 * frameX), int(0.8 * frameY)), (int(0.55 * frameX), int(0.85 * frameY))]
+      cv2.line(frame, line[0], line[1], (255, 255, 255), 2)   #(image, start_point, end_point, color, thickness)
+      line2 = [(int(0.05 * frameX), int(0.6 * frameY)), (int(0.2 * frameX), int(0.65 * frameY))]
+      cv2.line(frame, line2[0], line2[1], (255, 255, 255), 2)   #(image, start_point, end_point, color, thickness)
+    
       for track in tracker.tracks:
         if not track.is_confirmed() or track.time_since_update > 1:
           continue

9350f861b627ba3b90708c51e597ad7f2560dcdf
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-10-23 23:10:46 +0700

    Update newLine.py

--- tools/newLine.py
+++ tools/newLine.py
@@ -1,3 +1,15 @@
-def createLine(frame)
 
-def calLine(mulX,mulY)
+from collections import Counter
+
+def createLine(frame)
+  frameY = frame.shape[0] #360
+  frameX = frame.shape[1] #640
+def calLine(frame,mulX1,mulX2,mulY1,mulY2)
+  frameY = frame.shape[0] #360
+  frameX = frame.shape[1] #640
+  line = [(int(mulX1 * frameX), int(mulY1 * frameY)), (int(mulX2 * frameX), int(mulY2 * frameY))]
+  total_counter = 0
+  class_counter = Counter()
+  intersect_info = []
+  memory = {}
+  

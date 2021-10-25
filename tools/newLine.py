1f1865411a235d17eaa0417455fe00875f9e58b0
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-10-25 09:05:22 +0700

    Update newLine.py

--- tools/newLine.py
+++ tools/newLine.py
@@ -1,10 +1,7 @@
 
 from collections import Counter
 
-def createLine(frame)
-  frameY = frame.shape[0] #360
-  frameX = frame.shape[1] #640
-def calLine(frame,mulX1,mulX2,mulY1,mulY2)
+def createLine(frame,mulX1,mulX2,mulY1,mulY2)
   frameY = frame.shape[0] #360
   frameX = frame.shape[1] #640
   line = [(int(mulX1 * frameX), int(mulY1 * frameY)), (int(mulX2 * frameX), int(mulY2 * frameY))]

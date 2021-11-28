fbc7b48912771d2ff36d9b482e67c9a871479b67
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-11-28 08:43:10 +0700

    Update newLine.py

--- tools/newLine.py
+++ tools/newLine.py
@@ -17,14 +17,14 @@ def createLine2():
   numLine = int(input("Enter number of counting line :"))
   x = []
   y = []
+  print("Enter the beginning and ending of the line (0-1) (xb,yb,xe,ye) :")
   for l in range(numLine):
-    print("Enter the beginning and ending of the line (0-1) (x,y) :")
     print("Line",l+1)
     Bp = input("Enter line position :").split(",")
     x.append((Bp[0]))
     y.append((Bp[1]))
     x.append((Bp[2]))
-    y.append((Bp[2]))
+    y.append((Bp[3]))
   return numLine,x,y
 
 def createLineSpeed():

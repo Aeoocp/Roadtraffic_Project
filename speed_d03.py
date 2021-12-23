4dcf5ddd5fcd9f550f2e61b87840b8eb1c8beda4
Aeoocp <90436969+Aeocp@users.noreply.github.com>
2021-12-24 00:58:16 +0700

    Update speed_d03.py

--- speed_d03.py
+++ speed_d03.py
@@ -48,48 +48,30 @@ def main(_argv):
   metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
   tracker = Tracker(metric)
 
-  show_detections = True
-  writeVideo_flag = True
-  asyncVideo_flag = False
-
   file_path = FLAGS.video
-  if asyncVideo_flag:
-    video_capture = VideoCaptureAsync(file_path)  
-  else:
-    video_capture = cv2.VideoCapture(file_path)
+  video_capture = cv2.VideoCapture(file_path)
 
-  if asyncVideo_flag:
-    video_capture.start()
-
-  if writeVideo_flag:
-    if asyncVideo_flag:
-      w = int(video_capture.cap.get(3))
-      h = int(video_capture.cap.get(4))
-    else:
-      w = int(video_capture.get(3))
-      h = int(video_capture.get(4))
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(FLAGS.output, fourcc, 30, (w, h))
-    frame_index = -1
+  w = int(video_capture.get(3))
+  h = int(video_capture.get(4))
+  fourcc = cv2.VideoWriter_fourcc(*'XVID')
+  out = cv2.VideoWriter(FLAGS.output, fourcc, 30, (w, h))
+  frame_index = -1
 
   fps = 0.0
   fps_imutils = imutils.video.FPS().start()
   current_date = datetime.datetime.now().date()
   
-  total_counter = []
-  class_counter = []  # store counts of each detected class
+  line_tc = []    # นับจำนวนIDที่ผ่านแต่ละเส้น
   intersect_info = [] # initialise intersection list
   for ll in range(l):
-    total_counter.append(0)
-    class_counter.append(Counter())
+    line_tc.append([0,0]) # นับจำนวน ID ที่ตรวจจับได้ของแต่ละเส้น
     intersect_info.append([])
-  already_counted1 = deque(maxlen=50) # temporary memory for storing counted IDs forLine1
-  already_counted2 = deque(maxlen=50) # temporary memory for storing counted IDs forLine2
-  memory = {}
-  speed_mem = {}
-  speed_avg_list = []
-  speed_avg_memo = []
-  speed_avg = 0
+  line1_ac = deque(maxlen=50) # temporary memory for storing counted IDs forLine1
+  line2_ac = deque(maxlen=50) # temporary memory for storing counted IDs forLine2
+  memory = {}     # เก็บว่าเคยพิจารณาIDนี้ไปหรือยัง + ไว้เก็บmidpointไม่เกิน 2 จุด
+  time_mem = {}   # เก็บframeที่IDนั้นๆผ่านของแต่ละเส้น
+  speed_list = [] # ลิสความเร็วทั้งหมดที่คำนวณได้
+  speed_avg = 0   # ค่าเฉลี่ยความเร็วทั้งหมด
   
   ret, frame = video_capture.read()  # frame shape 640*480*3
   #สร้างเส้นผ่าน
@@ -97,6 +79,7 @@ def main(_argv):
   frameX = frame.shape[1] #640
   line1 = []
   line2 = []
+  #สร้างเส้น1,2ของถนนแต่ละเส้น
   for ll in range(l):
     xb1 = float(x1[ll*2])
     yb1 = float(y1[ll*2])
@@ -112,23 +95,19 @@ def main(_argv):
     line2.append(line_c2)
     
   while True:
-    print("frame", frame_index+1)
-    if ((frame_index+1)%600==0):
-      speed_avg_memo.append(speed_avg)
-      speed_avg_list = []
+#    print("frame", frame_index+1)
     ret, frame = video_capture.read()  # frame shape 640*480*3
-
+  
     if ret != True:
       break
  
-    #วาดเส้นผ่าน
+    # วาดเส้นทั้งหมดลงใน frame
     for ll in range(l):
       line_1 = line1[ll]
       cv2.line(frame, line_1[0], line_1[1], (255, 255, 255), 2)
       line_2 = line2[ll]
       cv2.line(frame, line_2[0], line_2[1], (255, 255, 255), 2)
     
-    b_size = 0
     t1 = time.time()
     
     image = Image.fromarray(frame[..., ::-1])  # bgr to rgb
@@ -136,7 +115,6 @@ def main(_argv):
       boxes = boxes_s[frame_index+1]
       confidence = confidence_s[frame_index+1]
       classes = classes_s[frame_index+1]
-    b_size = len(boxes)
 
     features = encoder(frame, boxes)
     # represents a bounding box detection in a single image
@@ -159,7 +137,7 @@ def main(_argv):
       bbox = track.to_tlbr()    # (min x, miny, max x, max y)
       track_cls = track.cls
 
-      midpoint = track.tlbr_midpoint(bbox)
+      midpoint = track.tlbr_midpoint(bbox)    
       # get midpoint respective to botton-left
       origin_midpoint = (midpoint[0], frame.shape[0] - midpoint[1])
 
@@ -170,50 +148,55 @@ def main(_argv):
       previous_midpoint = memory[track.track_id][0]
       origin_previous_midpoint = (previous_midpoint[0], frame.shape[0] - previous_midpoint[1])
       speedList = []
+      
+      #วนเช็คการตัดในแต่ละเส้น
       for ll in range(l):
         line_o = line1[ll]
+        # เช็คการตัดเส้น
         TC1 = CheckCrossLine.LineCrossing(midpoint, previous_midpoint, line_o[0] ,line_o[1])
-        if TC1 and (track.track_id not in already_counted1):
-          if track.track_id not in speed_mem:
-            speed_mem[track.track_id] = deque(maxlen=2)
-            speed_mem[track.track_id].append(frame_index+1)
-          class_counter[ll][track_cls] += 1
-          total_counter[ll] += 1
+        #ถ้าตัดและไม่เคยผ่านเส้น1
+        if TC1 and (track.track_id not in line1_ac):
+          if track.track_id not in time_mem:
+            time_mem[track.track_id] = []
+          time_mem[track.track_id].append(frame_index+1)
+          line_tc[ll][0] += 1
           # draw alert line
           cv2.line(frame, line_o[0], line_o[1], (0, 0, 255), 2)
-          already_counted1.append(track.track_id)  # Set already counted for ID to true.
-          intersection_time = datetime.datetime.now() - datetime.timedelta(microseconds=datetime.datetime.now().microsecond)
-          intersect_info[ll].append([track_cls, origin_midpoint, intersection_time])
-        line_o = line2[ll]
-        TC2 = CheckCrossLine.LineCrossing(midpoint, previous_midpoint, line_o[0] ,line_o[1])
-        if TC2 and (track.track_id not in already_counted2):
-          if track.track_id not in speed_mem:
-            speed_mem[track.track_id].append(frame_index+1)
-          class_counter[ll][track_cls] += 1
-          total_counter[ll] += 1
-          # draw alert line
-          cv2.line(frame, line_o[0], line_o[1], (0, 0, 255), 2)
-          already_counted2.append(track.track_id)  # Set already counted for ID to true.
+          line1_ac.append(track.track_id)  # เช็คว่า ID นี้ผ่านเส้นนี้แล้ว
           intersection_time = datetime.datetime.now() - datetime.timedelta(microseconds=datetime.datetime.now().microsecond)
           intersect_info[ll].append([track_cls, origin_midpoint, intersection_time])
         
-        print(type(speed_mem[track.track_id]))
-        trackTime1 = speed_mem[track.track_id].popleft()
-        trackTime2 = speed_mem[track.track_id].popleft()
-        distance = 4.5 #ระยะทางหน่วยเมตร 
-        time_tract = (trackTime2-trackTime1)/30 #เวลาในหน่วยวินาที
-        speed = distance/time_tract
-        speed_avg_list.append(speed)
+        line_o = line2[ll]
+        TC2 = CheckCrossLine.LineCrossing(midpoint, previous_midpoint, line_o[0] ,line_o[1])
+        if TC2 and (track.track_id not in line2_ac):
+          if track.track_id not in time_mem:
+            time_mem[track.track_id] = []
+          time_mem[track.track_id].append(frame_index+1)
+          line_tc[ll][1] += 1
+          # draw alert line
+          cv2.line(frame, line_o[0], line_o[1], (0, 0, 255), 2)
+          line2_ac.append(track.track_id)  # Set already counted for ID to true.
+          intersection_time = datetime.datetime.now() - datetime.timedelta(microseconds=datetime.datetime.now().microsecond)
+          intersect_info[ll].append([track_cls, origin_midpoint, intersection_time])
+        
+        if len(time_mem[track.track_id]) == 2 
+          time1 = time_mem[track.track_id][0]
+          time2 = time_mem[track.track_id][1]
+          distance = 40 #ระยะทางหน่วยเมตร 
+          realtime = (time2-time1)/30 # แปลงเวลาในหน่วยเฟรมเป็นวินาที
+          speed = (distance/realtime)*3.6 # คำนวณและแปลงหน่วยเป็นกิโลเมตรต่อชั่วโมง
+          speed_list.append(speed)
+          savg = 0
+          co = len(speed_list)
+          for s in speed_list
+            savg += s
+          speed_avg = s/co
+          print("Frame:",frame_index ," ID:" ,track.track_id ," speed:" ,speed)
           
-      if track_cls == "car":
-        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
-        cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 1.5e-3 * frame.shape[0], (0, 255, 0), 2)
-        cv2.putText(frame, str(track_cls), (int(bbox[0]), int(bbox[3])), 0, 1.5e-3 * frame.shape[0], (0, 255, 0), 2)
-      else:
-        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
-        cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1]-25)), 0, 1.5e-3 * frame.shape[0], (0, 0, 255), 2)
-        cv2.putText(frame, str(track_cls), (int(bbox[0]), int(bbox[3])), 0, 1.5e-3 * frame.shape[0], (0, 0, 255), 2)
-
+      cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+      cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0, 1.5e-3 * frame.shape[0], (0, 255, 0), 2)
+      if track.track_id in speed_list:
+        cv2.putText(frame, str(speed_list[track.track_id]), (int(bbox[2]), int(bbox[1])), 0, 1.5e-3 * frame.shape[0], (0, 0, 255), 2)
     # Delete memory of old tracks.
     # This needs to be larger than the number of tracked objects in the frame.  
     if len(memory) > 50:
@@ -223,31 +206,21 @@ def main(_argv):
     yy = 0.1 * frame.shape[0]
     for ll in range(l):
       xx = ll+1
-      cv2.putText(frame, "Total{}: {}".format(str(xx),str(total_counter[ll])), (int(0.05 * frame.shape[1]), int(yy)), 0,
+      cv2.putText(frame, "Total{}: {},{}".format(str(xx),str(line_tc[ll][0]),str(line_tc[ll][1])), (int(0.05 * frame.shape[1]), int(yy)), 0,
                 1.5e-3 * frame.shape[0], (0, 255, 255), 2)
       yy += 0.1 * frame.shape[0]
-      print("Total",xx,": ",total_counter[ll])
+      print("Total",xx,": ",line_tc[ll])
       
     cv2.putText(frame, "frame_index {}".format(str(frame_index+1)), (int(0.5 * frame.shape[1]), int(0.9 * frame.shape[0])), 0,
                   1.5e-3 * frame.shape[0], (255, 255, 255), 2)
+    cv2.putText(frame, "speed_avg {}".format('%.2f' % speed_avg), (int(0.5 * frame.shape[1]), int(0.1 * frame.shape[0])), 0,
+                  1.5e-3 * frame.shape[0], (255, 255, 255), 2)
     
-    speed_size = len(speed_avg_list)
-    sum = 0
-    for s in speed_avg_list:
-      sum += int(s) 
-    if(speed_size != 0):
-      speed_avg = sum/speed_size
-    print("speed_avg : ",speed_avg)
-    
-    if writeVideo_flag:
-        # save a frame
-        out.write(frame)
-        frame_index = frame_index + 1
+    out.write(frame)
+    frame_index = frame_index + 1
 
     fps_imutils.update()
-
-    if not asyncVideo_flag:
-      fps = (fps + (1. / (time.time() - t1))) / 2
+    fps = (fps + (1. / (time.time() - t1))) / 2
 
     # Press Q to stop!
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -256,14 +229,8 @@ def main(_argv):
   fps_imutils.stop()
   print('imutils FPS: {}'.format(fps_imutils.fps()))
 
-  if asyncVideo_flag:
-    video_capture.stop()
-  else:
-    video_capture.release()
-
-  if writeVideo_flag:
-        out.release()
-
+  video_capture.release()
+  out.release()
   cv2.destroyAllWindows()
     
 if __name__ == '__main__':
